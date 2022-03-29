a9e5dfad030a5e4c4476606483c26248c8b98741
Esteban <estibex12@gmail.com>
2022-03-28 19:25:55 -0500

    Add project files.

---
+++ PressureLogger_WPFTest2/Model/PressureTestGeneralData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressureLogger_WPFTest2.Model
+{
+    public class PressureTestGeneralData
+    {
+
+        private ClientData clientData;
+        private EquipmentData equipmentData;
+        private PressureGraphData graphData;
+        private TransducerData transducerData;
+
+        public PressureTestGeneralData() 
+        {
+            
+        }
+
+    }
+}
